49d5682d9416f30d52ccf2cb5d043e601f00b30a
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-02 18:34:02 +0500

    Initialize FastAPI application with basic routing and configuration

---
+++ services/api/app/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from typing import Any, Dict, Union
+from arq.connections import RedisSettings
+load_dotenv()
+
+
+class ApiServiceConfig:
+    BASE_PREFIX = '/api/v1'
+    DEFAULT_RESPONSE: Dict[Union[int,str], Dict[str, Any]] = {200: {"description": "Successful Response"}}
+
+class RedisConfig:
+    REDIS_SETTINGS = RedisSettings(os.getenv('REDIS_HOST', 'localhost'), int(os.getenv('REDIS_PORT', '6379')))
---
+++ services/api/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from .routers import admin, balance, order, public
+from .config import ApiServiceConfig
+
+
+app = FastAPI()
+app.include_router(public.router, prefix=ApiServiceConfig.BASE_PREFIX)
+app.include_router(balance.router, prefix=ApiServiceConfig.BASE_PREFIX)
+app.include_router(order.router, prefix=ApiServiceConfig.BASE_PREFIX)
+app.include_router(admin.router, prefix=ApiServiceConfig.BASE_PREFIX)
+

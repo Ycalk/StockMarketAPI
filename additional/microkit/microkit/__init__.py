63048e151ef51cc0c2f1db1aa7579efc5539c13f
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-03 23:20:23 +0500

    Add docs for module

---
+++ additional/microkit/microkit/__init__.py
@@ -0,0 +1,9 @@
+from .client import MicroKitClient
+from .service import Service, Runner, service_method
+
+__all__ = [
+    "MicroKitClient",
+    "Service",
+    "Runner",
+    "service_method",
+]

2b92f4386821c2a40c4dd683709d512ca4b252e8
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-03 22:27:58 +0500

    Implement create_user method to handle user creation with CreateUserRequest

--- services/users/src/users.py
+++ services/users/src/users.py
@@ -3,6 +3,9 @@ from tortoise import Tortoise
 from .config import TORTOISE_ORM
 from microkit.service import Service, service_method
 from arq.connections import ArqRedis
+from shared_models.users import User as UserSharedModel
+from shared_models.users.create_user import CreateUserRequest
+from .models import User
 
 
 class Users(Service):
@@ -15,6 +18,8 @@ class Users(Service):
 
     # Methods
     @service_method
-    async def create_user(self: "Users", redis: ArqRedis):
-        pass
+    async def create_user(self: "Users", redis: ArqRedis, request: CreateUserRequest) -> UserSharedModel:
+        user = await User.create(**request.model_dump(exclude_unset=True))
+        self.logger.info(f"User created with ID: {user.id}")
+        return UserSharedModel.model_validate(user)
     

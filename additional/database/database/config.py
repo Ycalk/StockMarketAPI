902754ee748c2b9a4c8f5cd1340b19cb73e89694
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-14 21:49:05 +0500

    Move database to additional package

---
+++ additional/database/database/config.py
@@ -0,0 +1,22 @@
+import os
+
+TORTOISE_ORM = {
+    'connections': {
+        'default': {
+            'engine': 'tortoise.backends.asyncpg',
+            'credentials': {
+                'host': os.getenv('DB_HOST', 'localhost'),
+                'port': os.getenv('DB_PORT', '5432'),
+                'user': os.getenv('DB_USER'),
+                'password': os.getenv('DB_PASSWORD'),
+                'database': os.getenv('DB_NAME'),
+            }
+        },
+    },
+    'apps': {
+        'models': {
+            'models': ['src.models', 'aerich.models'],
+            'default_connection': 'default',
+        }
+    }
+}

8a96dd2505261d6c325b0a13e9b5d537a28ab123
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-15 22:59:32 +0500

    Add instrument management endpoints to admin and public routers

--- services/api/app/routers/admin.py
+++ services/api/app/routers/admin.py
@@ -1,4 +1,26 @@
 from fastapi import APIRouter
+from microkit import MicroKitClient
+from ..config import RedisConfig
+from shared_models.instruments.add_instrument import AddInstrumentResponse
+from shared_models.instruments import Instrument as InstrumentSharedModel
+from shared_models.instruments.delete_instrument import DeleteInstrumentRequest, DeleteInstrumentResponse
 
 
 router = APIRouter(prefix="/admin", tags=["admin"])
+instruments_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Instruments")
+
+
+@router.post("/instrument", response_model=AddInstrumentResponse)
+async def create_instrument(request: InstrumentSharedModel):
+    job = await instruments_client("add_instrument", request)
+    if job is None:
+        raise ValueError("Job is None")
+    return await job.result()
+
+
+@router.delete("/instrument/{ticker}", response_model=DeleteInstrumentResponse)
+async def delete_instrument(ticker: str):
+    job = await instruments_client("delete_instrument", DeleteInstrumentRequest(ticker=ticker))
+    if job is None:
+        raise ValueError("Job is None")
+    return await job.result()
--- services/api/app/routers/public.py
+++ services/api/app/routers/public.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
-from shared_models.users import User as UserSharedModel
-from shared_models.users.create_user import CreateUserRequest
 from ..models.public import RegisterUserRequest, RegisterUserResponse
 from microkit.client import MicroKitClient
 from ..config import RedisConfig
+from shared_models.users import User as UserSharedModel
+from shared_models.users.create_user import CreateUserRequest
+from shared_models.instruments.get_instruments import GetInstrumentsResponse
 
 
 router = APIRouter(prefix="/public", tags=["public"])
 users_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Users")
+instruments_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Instruments")
 
 
 @router.post("/register", response_model=RegisterUserResponse)
@@ -17,3 +19,11 @@ async def register_user(request: RegisterUserRequest):
         raise ValueError("Job is None")
     model: UserSharedModel = await job.result()
     return RegisterUserResponse(**model.model_dump())
+
+
+@router.get("/instrument", response_model=GetInstrumentsResponse)
+async def get_instruments():
+    job = await instruments_client("get_instruments")
+    if job is None:
+        raise ValueError("Job is None")
+    return await job.result()

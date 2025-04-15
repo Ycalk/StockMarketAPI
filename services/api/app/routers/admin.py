fd8c9446abd77de2c4033d7604c22cff71b6e6d2
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-15 23:56:27 +0500

    Add delete user endpoint to admin router

--- services/api/app/routers/admin.py
+++ services/api/app/routers/admin.py
@@ -4,10 +4,24 @@ from ..config import RedisConfig
 from shared_models.instruments.add_instrument import AddInstrumentResponse
 from shared_models.instruments import Instrument as InstrumentSharedModel
 from shared_models.instruments.delete_instrument import DeleteInstrumentRequest, DeleteInstrumentResponse
+from shared_models.users import User as UserSharedModel
+from shared_models.users.delete_user import DeleteUserRequest
+from ..models.user import User as DeleteUserResponse
+from uuid import UUID
 
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 instruments_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Instruments")
+users_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Users")
+
+
+@router.delete("/user/{user_id}", response_model=DeleteUserResponse)
+async def delete_user(user_id: UUID):
+    job = await users_client("delete_user", DeleteUserRequest(id=user_id))
+    if job is None:
+        raise ValueError("Job is None")
+    model: UserSharedModel = await job.result()
+    return DeleteUserResponse(**model.model_dump())
 
 
 @router.post("/instrument", response_model=AddInstrumentResponse)

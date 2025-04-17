5542250fa09f322f6fc6d91793dab11804eaf1d9
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-04-17 15:00:48 +0500

    Update api to match to request/response models

--- services/api/app/routers/admin.py
+++ services/api/app/routers/admin.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter
 from microkit import MicroKitClient
 from ..config import RedisConfig
-from shared_models.instruments.add_instrument import AddInstrumentResponse
+from shared_models.instruments.add_instrument import AddInstrumentRequest
 from shared_models.instruments import Instrument as InstrumentSharedModel
-from shared_models.instruments.delete_instrument import DeleteInstrumentRequest, DeleteInstrumentResponse
-from shared_models.users import User as UserSharedModel
-from shared_models.users.delete_user import DeleteUserRequest
+from shared_models.instruments.delete_instrument import DeleteInstrumentRequest
+from shared_models.users.delete_user import DeleteUserRequest, DeleteUserResponse
 from shared_models.users.deposit import DepositRequest
 from shared_models.users.withdraw import WithdrawRequest
-from ..models.user import User as DeleteUserResponse
+from ..models.user import User as UserAPIModel
 from ..models.response_status import ResponseStatus
 from ..services.token import verify_admin_api_key
 from fastapi import Depends
@@ -20,29 +19,35 @@ instruments_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Instruments")
 users_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Users")
 
 
-@router.delete("/user/{user_id}", response_model=DeleteUserResponse)
+@router.delete("/user/{user_id}", response_model=UserAPIModel)
 async def delete_user(user_id: UUID, _:None = Depends(verify_admin_api_key)):
     job = await users_client("delete_user", DeleteUserRequest(id=user_id))
     if job is None:
         raise ValueError("Job is None")
-    model: UserSharedModel = await job.result()
-    return DeleteUserResponse(**model.model_dump())
+    model: DeleteUserResponse = await job.result()
+    return UserAPIModel(**model.user.model_dump())
 
 
-@router.post("/instrument", response_model=AddInstrumentResponse)
+@router.post("/instrument", response_model=ResponseStatus)
 async def create_instrument(request: InstrumentSharedModel, _:None = Depends(verify_admin_api_key)):
-    job = await instruments_client("add_instrument", request)
-    if job is None:
-        raise ValueError("Job is None")
-    return await job.result()
+    try:
+        job = await instruments_client("add_instrument", AddInstrumentRequest(instrument=request))
+        if job is None:
+            raise ValueError("Job is None")
+        return ResponseStatus(success=True)
+    except Exception as e:
+        return ResponseStatus(success=False)
 
 
-@router.delete("/instrument/{ticker}", response_model=DeleteInstrumentResponse, tags=["user"])
+@router.delete("/instrument/{ticker}", response_model=ResponseStatus, tags=["user"])
 async def delete_instrument(ticker: str, _:None = Depends(verify_admin_api_key)):
-    job = await instruments_client("delete_instrument", DeleteInstrumentRequest(ticker=ticker))
-    if job is None:
-        raise ValueError("Job is None")
-    return await job.result()
+    try:
+        job = await instruments_client("delete_instrument", DeleteInstrumentRequest(ticker=ticker))
+        if job is None:
+            raise ValueError("Job is None")
+        return ResponseStatus(success=True)
+    except Exception as e:
+        return ResponseStatus(success=False)
 
 
 @router.post("/balance/deposit", response_model=ResponseStatus, tags=["balance"])
--- services/api/app/routers/public.py
+++ services/api/app/routers/public.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 from ..models.public import RegisterUserRequest
-from ..models.user import User as RegisterUserResponse
+from ..models.user import User as UserAPIModel
 from microkit.client import MicroKitClient
 from ..config import RedisConfig
-from shared_models.users import User as UserSharedModel
-from shared_models.users.create_user import CreateUserRequest
+from shared_models.users.create_user import CreateUserRequest, CreateUserResponse
 from shared_models.instruments.get_instruments import GetInstrumentsResponse
 
 
@@ -13,13 +12,13 @@ users_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Users")
 instruments_client = MicroKitClient(RedisConfig.REDIS_SETTINGS, "Instruments")
 
 
-@router.post("/register", response_model=RegisterUserResponse)
+@router.post("/register", response_model=UserAPIModel)
 async def register_user(request: RegisterUserRequest):
     job = await users_client("create_user", CreateUserRequest(**request.model_dump()))
     if job is None:
         raise ValueError("Job is None")
-    model: UserSharedModel = await job.result()
-    return RegisterUserResponse(**model.model_dump())
+    model: CreateUserResponse = await job.result()
+    return UserAPIModel(**model.user.model_dump())
 
 
 @router.get("/instrument", response_model=GetInstrumentsResponse)

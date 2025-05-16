0d9ee978438b9d2d6e7bf5b4035c2ddd1bdda639
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-05-17 02:09:38 +0500

    Fix api running host

--- services/api/run.py
+++ services/api/run.py
@@ -5,4 +5,4 @@ from app.main import app
 port = os.getenv("SERVER_PORT", "5000")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=int(port))
+    uvicorn.run(app, host="0.0.0.0", port=int(port))

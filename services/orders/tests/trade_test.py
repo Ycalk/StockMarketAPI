9d335cb8d18478d90dba37355f0b09819bef6dbd
Ycalk <118367882+Ycalk@users.noreply.github.com>
2025-05-17 23:18:32 +0500

    Change test to check if raises InsufficientFundsError when buying

--- services/orders/tests/trade_test.py
+++ services/orders/tests/trade_test.py
@@ -10,6 +10,7 @@ from shared_models.orders.models.orders_bodies.direction import (
 )
 from shared_models.orders.models.order_status import OrderStatus
 from shared_models.orders.requests.get_order import GetOrderRequest
+from shared_models.users.errors import InsufficientFundsError
 
 
 @pytest.mark.asyncio
@@ -290,7 +291,7 @@ async def test_limit_orders_not_matched_due_to_price(
 
 
 @pytest.mark.asyncio
-async def test_not_full_execute_cause_buyer_have_not_got_enough_rub(
+async def test_raises_insufficient_funds_on_buy(
     ctx: dict, instrument: Instrument, rub: Instrument
 ):
     seller = await User.create(name="Seller")
@@ -299,7 +300,7 @@ async def test_not_full_execute_cause_buyer_have_not_got_enough_rub(
     await Balance.create(user=seller, instrument=instrument, amount=10)
     await Balance.create(user=buyer, instrument=rub, amount=100)
 
-    buy_order_id = (
+    with pytest.raises(InsufficientFundsError):
         await Orders.create_order(
             ctx,
             CreateOrderRequest(
@@ -310,52 +311,8 @@ async def test_not_full_execute_cause_buyer_have_not_got_enough_rub(
                     qty=10,
                     price=100,
                 ),
-            ),
-        )
-    ).order_id
-
-    sell_order_id = (
-        await Orders.create_order(
-            ctx,
-            CreateOrderRequest(
-                user_id=seller.id,
-                body=LimitOrderBody(
-                    direction=SharedModelOrderDirection.SELL,
-                    ticker=instrument.ticker,
-                    qty=10,
-                    price=100,
-                ),
-            ),
-        )
-    ).order_id
-
-    buy_order = (
-        await Orders.get_order(
-            ctx, GetOrderRequest(user_id=buyer.id, order_id=buy_order_id)
-        )
-    ).root
-    sell_order = (
-        await Orders.get_order(
-            ctx, GetOrderRequest(user_id=seller.id, order_id=sell_order_id)
-        )
-    ).root
-
-    assert buy_order.filled == 1
-    assert buy_order.status == OrderStatus.NEW
-
-    assert sell_order.filled == 1
-    assert sell_order.status == OrderStatus.NEW
-
-    seller_rub_balance = await Balance.get(user=seller, instrument=rub)
-    buyer_rub_balance = await Balance.get(user=buyer, instrument=rub)
-
-    seller_instrument_balance = await Balance.get(user=seller, instrument=instrument)
-    buyer_instrument_balance = await Balance.get(user=buyer, instrument=instrument)
-
-    assert buyer_instrument_balance.amount == 1
-    assert seller_instrument_balance.amount == 9
-    assert seller_rub_balance.amount == 100
-    assert buyer_rub_balance.amount == 0
+        ),
+    )
 
 
 @pytest.mark.asyncio
